211cb6e0ac9a5149d384eb7b2d968901ba7cac69
Ruben Abergel <ruben.abergel@gmail.com>
2015-04-20 19:26:40 -0700

    mongoose + connect to compose

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://rubenabergel:qwertyuiop@dogen.mongohq.com:10047/whiteboardDB');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var Room = mongoose.model('Room', { name: String, drawingPoints : [] });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+    console.log('saved');
+  if (err)
+  console.log('meow');
+});
+
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
